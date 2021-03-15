bf5f3054b1ea574f658f5344d98522abb62570b9
James McCollum <jmccollum@signet-studios.com>
2021-03-15 05:56:35 -0500

    Increase homing feedrate closer to stock

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1588,7 +1588,7 @@
 #endif
 
 // Homing speeds (mm/min)
-#define HOMING_FEEDRATE_MM_M { (20*60), (20*60), (4*60) }
+#define HOMING_FEEDRATE_MM_M { (40*60), (40*60), (4*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
